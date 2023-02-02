f62407393c3fe6c5e1e5624e2a33bc73eb8041cc
MaxWattan <mcwattanac@gmail.com>
2023-02-01 20:29:13 -0500

    finished lab

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,3 +1,15 @@
-export default function Progress() {
-  return null;
+export default function Progress(props) {
+  //accumulator loop for amount
+  let amount = 0
+  for (let i=0; i < props.donations.length; i++){
+amount += props.donations[i].amount;
+  }
+  return (
+    <section className="progress">
+  <h2>
+    Raised <span className="secondary">${amount}</span> of
+    <span className="secondary"> $1000</span>
+  </h2>
+</section>
+  )
 }
--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,3 +1,18 @@
-export default function RecentDonations() {
-  return null;
+
+export default function RecentDonations(props) {
+  const donations = props.donations;
+  const listDonations = donations.map(donation => {
+    return (
+    <li><span>{donation.name} donated ${donation.amount}</span>{donation.caption}</li>
+    )
+  })
+
+  return (
+    <section>
+  <h2>Recent Donations</h2>
+  <ul>
+    {listDonations}
+  </ul>
+</section>
+  )
 }
