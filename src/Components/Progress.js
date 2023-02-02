4a41ef67e5d94035ba699ec50e4ede352189c6bd
Angel <EMAIL>
2023-02-01 21:22:57 -0500

    props

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,3 +1,9 @@
-export default function Progress() {
-  return null;
+export default function Progress({total, actualAmount}) {
+  return (
+    <section>
+    <h2> 
+      Raised <span className='secondary'>${total}</span> of <span className='secondary'>${actualAmount}</span>
+    </h2>
+    </section>
+  )
 }
