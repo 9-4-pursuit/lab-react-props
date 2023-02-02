9d0a0d3cb6f3fa03b8c682be775ec9316c53d78c
Diegocarrero <diegocar2413@gmail.com>
2023-02-01 20:13:31 -0500

    Pass all tests

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,3 +1,16 @@
-export default function Progress() {
-  return null;
+export default function Progress(props) {
+  let amount = 0;
+  for (const donation of props.donations) {
+    amount += donation.amount;
+  }
+  return (
+    <div>
+      <section className="progress">
+        <h2>
+          Raised <span className="secondary">${amount}</span> of
+          <span className="secondary"> ${props.targetAmount}</span>
+        </h2>
+      </section>
+    </div>
+  )
 }
--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,3 +1,19 @@
-export default function RecentDonations() {
-  return null;
+
+
+export default function RecentDonations(props) {
+
+  const recentDonations = props.donations.map(donation => {
+    return (
+      <li><span>{donation.name} donated ${donation.amount}</span>{donation.caption}</li>
+    )
+  })
+
+  return (
+    <div>
+      <section>
+        <h2>Recent Donations</h2>
+        <ul>{recentDonations}</ul>
+      </section>
+    </div>
+  )
 }
