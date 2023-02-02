913f48ea349da359b20593c60ea70d57426787a3
arerimr <raydelys.morrobelr@gmail.com>
2023-02-01 20:43:22 -0500

    Props Lab

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import TopBar from "./Components/TopBar";
+import DonationForm from "./Components/DonationForm";
+import Progress from "./Components/Progress";
+import RecentDonations from "./Components/RecentDonations";
 import "./App.css";
 
 const targetAmount = 1000;
@@ -36,15 +39,40 @@ const donations = [
   },
 ];
 
+const sumOfAmounts = (donations) => {
+  let sum = 0
+  for (let i=0; i < donations.length; i++){
+    sum += donations[i].amount
+  }
+  return sum
+}
+// let name, amount, caption
+// for (let i=0; i < donations.length; i++){
+//   name = donations[i].name
+//   amount = donations[i].amount
+//   caption = donations[i].caption
+// }
+
+
+
 function App() {
   return (
     <>
       <TopBar />
       <main className="container">
-        <section className="sidebar">{/* Recent Donations */}</section>
+        <section className="sidebar">
+          <h2>Recent Donations</h2>
+          <RecentDonations name={donations[0].name} amount={donations[0].amount} caption={donations[0].caption}/>
+          <RecentDonations name={donations[1].name} amount={donations[1].amount} caption={donations[1].caption}/>
+          <RecentDonations name={donations[2].name} amount={donations[2].amount} caption={donations[2].caption}/>
+          <RecentDonations name={donations[3].name} amount={donations[3].amount} caption={donations[3].caption}/>
+          <RecentDonations name={donations[4].name} amount={donations[4].amount} caption={donations[4].caption}/>
+        </section>
         <section className="">
           {/* Progress */}
+          <Progress targetAmount={targetAmount} raised={sumOfAmounts(donations)}/>
           {/* Donation Form */}
+          <DonationForm id={donations.length + 1} />
         </section>
       </main>
     </>
--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,3 +1,10 @@
-export default function Progress() {
-  return null;
+export default function Progress(props) {
+  return (
+    <section className="progress">
+      <h2>
+        Raised <span className="secondary">${props.raised}</span> of
+        <span className="secondary"> ${props.targetAmount}</span>
+      </h2>
+    </section>
+  );
 }
