27a8cd82e7a651bd5f43b7899b0fdee040167f26
JoseC620 <Jcepeda6201@gmail.com>
2023-02-01 20:09:04 -0500

    finished

--- src/Components/DonationForm.js
+++ src/Components/DonationForm.js
@@ -1,3 +1,26 @@
-export default function DonationForm() {
-  return null;
+export default function DonationForm(props) {
+  return (
+  <section className="donation">
+  <h3>You could be donation <span class="secondary">#{props.amtOfDonos}!</span></h3>
+  <form>
+    <label htmlFor="name">{props.name}
+    <input
+        id="name"
+        name="name"
+        type="text"
+        placeholder="Your name..." /></label>
+        <label htmlFor="caption">{props.caption}<input
+        id="caption"
+        name="caption"
+        type="text"
+        placeholder="Add a brief message..."/></label>
+        <label htmlFor="amount">{props.amount}<input
+        id="amount"
+        name="amount"
+        type="number"
+        placeholder="0" /></label>
+        <button>Donate!</button>
+  </form>
+</section>
+)
 }
--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,3 +1,14 @@
-export default function Progress() {
-  return null;
+export default function Progress(props) {
+  let totalDonate = 0
+  for (let donation of props.arr){
+totalDonate += donation.amount
+}
+  return (
+  <section className="progress">
+  <h2>
+    Raised <span className="secondary">${totalDonate}</span> of
+    <span className="secondary"> $1000</span>
+  </h2>
+</section>
+)
 }
