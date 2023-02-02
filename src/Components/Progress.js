eb4e34dfe1352d9f3b55b51fde9632691cc5c5b5
jRodriguezIV <j.a.rodrigueziv@gmail.com>
2023-02-01 20:23:49 -0500

    finished all tests

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,3 +1,23 @@
-export default function Progress() {
-  return null;
+import React from "react"
+
+
+export default function Progress(props) {
+  let num = 0
+  let donations = props.donations
+
+  donations.forEach(el => {
+    num += el.amount
+  })
+
+
+
+
+  return (
+    <section className="progress">
+      <h2>
+        Raised <span className="secondary">${num} </span> of
+        <span className="secondary"> ${props.targetAmount}</span>
+      </h2>
+    </section>
+  )
 }
