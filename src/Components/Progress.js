2df5491f67641c0687dfae8bb5a1b8a6676287c3
Felizj17 <felizj17@gmail.com>
2023-02-02 19:58:49 -0500

    props lab complete

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,3 +1,14 @@
-export default function Progress() {
-  return null;
+export default function Progress(props) {
+  let c = 0
+  props.donations.forEach(el => {
+    c += el.amount
+  })
+  return (
+    <section className='progress'>
+      <h2>
+        Raised <span className='secondary'>${c}</span> of{' '}
+        <span className='secondary'> ${props.targetAmount}</span>
+      </h2>
+    </section>
+  )
 }
--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,3 +1,17 @@
-export default function RecentDonations() {
-  return null;
+export default function RecentDonations(props) {
+  return (
+    <section>
+      <h2>Recent Donations</h2>
+      <ul>
+        {props.donations.map((el, i) => (
+          <li key={`list-${i}`}>
+            <span>
+              {el.name} donated ${el.amount}
+            </span>
+            {el.caption}
+          </li>
+        ))}
+      </ul>
+    </section>
+  )
 }
