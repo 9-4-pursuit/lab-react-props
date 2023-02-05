68f64d4dc4dafbf4ae9a35ed69f09f7a877c9203
USERNAME <EMAIL>
2023-02-04 19:25:47 -0500

    finished lab-react-props

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,3 +1,14 @@
-export default function Progress() {
-  return null;
-}
+export default function Progress(props) {
+  return (
+  
+<div className="progress">
+  <section >
+  <h2>
+    Raised <span className="secondary">${props.amount}</span> of 
+     <span className="secondary"> ${props.target }</span>
+  </h2>
+</section>
+</div>
+)
+
+  }
--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,3 +1,17 @@
-export default function RecentDonations() {
-  return null;
-}
+import DonationForm from "./DonationForm";
+
+export default function RecentDonations(props) {
+  return (
+
+<div>
+  <h2>Recent Donations</h2>
+  <ul>
+  <li><span>{props.donations[0].name} donated ${props.donations[0].amount}</span>{props.donations[0].caption}</li>
+  <li><span>{props.donations[1].name} donated ${props.donations[1].amount}</span>{props.donations[1].caption}</li>
+  <li><span>{props.donations[2].name} donated ${props.donations[2].amount}</span>{props.donations[2].caption}</li>
+  <li><span>{props.donations[3].name} donated ${props.donations[3].amount}</span>{props.donations[3].caption}</li>
+  <li><span>{props.donations[4].name} donated ${props.donations[4].amount}</span>{props.donations[4].caption}</li>
+  </ul>
+</div>
+)
+  }
