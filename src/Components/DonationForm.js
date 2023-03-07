a2ef72cee553095cb821c30185ec1dbd4652306c
SamuelBAlba <brayanbalba24@gmail.com>
2023-03-06 20:51:13 -0500

    lab done

--- src/Components/DonationForm.js
+++ src/Components/DonationForm.js
@@ -1,3 +1,42 @@
-export default function DonationForm() {
-  return null;
+import React from "react"
+
+export default function DonationForm(props) {
+
+  // let number = 1
+  // props.donations.forEach(el => number += 1)
+
+  return (
+    <section className="donation"> 
+      <h3>You could be donation <span class="secondary">#{props.donations.length + 1}!</span></h3>
+      <form>
+      
+      <label htmlFor="name">
+      Name <input
+        id="name"
+        name="name"
+        type="text"
+        placeholder="Your name..." /> 
+        </label>
+        
+    <label htmlFor="caption">
+      Caption <input
+        id="caption"
+        name="caption"
+        type="text"
+        placeholder="Add a brief message..." />
+        </label>
+
+    <label htmlFor="amount"
+      >Amount <input
+        id="amount"
+        name="amount"
+        type="number"
+        placeholder="0" />
+        </label>
+
+    <button>Donate!</button>
+      </form>
+    </section>
+  )
+ 
 }
